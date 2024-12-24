2781bbb49957ce9725f44983039ab041a4a90c7b
unknown <domipowski@gmail.com>
2024-12-23 20:03:52 -0500

    Fixed Routing problem and Babel dependency missing

--- toms-electric/src/NavBar.js
+++ toms-electric/src/NavBar.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Button, Container, Row, Col } from 'reactstrap';
-import { useNavigate, Link } from "react-router-dom";
+import { useNavigate } from "react-router-dom";
 
 function NavBar() {
   let navigate = useNavigate();
@@ -12,8 +12,8 @@ function NavBar() {
           
           {/* Logo */}
           <Col xs="12" md="4" className="d-flex align-items-center justify-content-center mb-3 mb-md-0">
-            <img src="/logo_only.png" alt="Tom's Electric Logo" className="logo-size clickable" onClick={() => navigate("/home")}/>
-            <div className="clickable" onClick={() => navigate("/home")}>
+            <img src="/logo_only.png" alt="Tom's Electric Logo" className="logo-size clickable" onClick={() => navigate("")}/>
+            <div className="clickable" onClick={() => navigate("")}>
               <h1 className="mb-0">Tom's</h1>
               <h1 className="mb-0">Electric</h1>
               <h1 className="mb-0">inc.</h1>
@@ -24,7 +24,7 @@ function NavBar() {
           <Col xs="12" md="8" className="d-flex justify-content-center justify-content-md-end mt-3 mt-md-0">
             <Button 
               className="mx-1 mx-md-2 nav-btns"
-              onClick={() => navigate("/home")}
+              onClick={() => navigate("")}
             >
               Home
             </Button>
